6bbc344b11d7408dcb53a7a909a13495f2f42b39
tetsbackend121 <169013733+tetsbackend121@users.noreply.github.com>
2024-06-07 21:38:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 80;
-const path = path();
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
