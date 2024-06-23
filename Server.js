6e27c1eaf1c9d4389850e1dec7a909dad314734b
tetsbackend121 <169013733+tetsbackend121@users.noreply.github.com>
2024-06-23 16:26:29 -0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -200,7 +200,7 @@ app.post('/enviar-mensaje', upload.single('audio'), async (req, res) => {
             console.log("TypeError occurred ya esta gil:");
         } else {
         
-            console.log("Unexpected error occurred caca:", error);
+            console.log("Unexpected error occurred caca:");
         }
     // Capturar y manejar cualquier error que ocurra durante el proceso
     
