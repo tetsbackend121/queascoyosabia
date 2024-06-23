d9b3e4ed79f94a9c0630e5fd47c2979ad033a878
tetsbackend121 <169013733+tetsbackend121@users.noreply.github.com>
2024-06-23 15:57:45 -0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -194,8 +194,16 @@ app.post('/enviar-mensaje', upload.single('audio'), async (req, res) => {
         console.log("Tipo: " + tipo);
 
 } catch (error) {
+        
+        if (error instanceof TypeError) {
+            
+            console.log(`TypeError occurred ya esta gil:);
+        } else {
+        
+            console.log(`Unexpected error occurred caca:);
+        }
     // Capturar y manejar cualquier error que ocurra durante el proceso
-    console.log("Error del orto", error);
+    
 }
 
 
